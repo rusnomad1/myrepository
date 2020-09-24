137a08f48c9ebd93fc7933ab515803196793640f
rusnomad1 <71275109+rusnomad1@users.noreply.github.com>
2020-09-24 16:59:56 +0600

    Updates

---
+++ precache-manifest.a3d021c1107282def9dd8a351b9d83b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97de5158c01fd673044a9b9bcf02ae79",
+    "url": "/myrepository/index.html"
+  },
+  {
+    "revision": "9b58922e616c8b9c2354",
+    "url": "/myrepository/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "4e22464cb0a28eb40533",
+    "url": "/myrepository/static/css/main.6cacf05b.chunk.css"
+  },
+  {
+    "revision": "9b58922e616c8b9c2354",
+    "url": "/myrepository/static/js/2.682dc5d1.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myrepository/static/js/2.682dc5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e22464cb0a28eb40533",
+    "url": "/myrepository/static/js/main.a34d6581.chunk.js"
+  },
+  {
+    "revision": "fd69cc56ef756aaa3188",
+    "url": "/myrepository/static/js/runtime-main.efa62bb9.js"
+  },
+  {
+    "revision": "c37c12ef9800dbf9e92ed4fbc2ef0e18",
+    "url": "/myrepository/static/media/мое фото.c37c12ef.jpg"
+  }
+]);
